d26033a97e7be5094cc005ef40ade2663872865a
dharshiniudhayakumar0 <dharshinithamarai0@gmail.com>
2026-04-19 22:55:05 +0530

    Total Reset: Stripped all dependencies to resolve Vercel crash

--- api/products.js
+++ api/products.js
@@ -6,15 +6,7 @@ const mockProducts = [
 ];
 
 module.exports = (req, res) => {
-    // Add CORS headers manually since we're not using the cors middleware
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(mockProducts);
 };
